6e8a7f836f2af1fd6991377b8a6d2f5cafbeb062
steve <steve@localhost>
1998-01-16 05:51:25 +0000

    Initial revision

---
+++ package-compile.el
@@ -0,0 +1,175 @@
+;;; package-compile.el --- Bytecompiler wrapper for building packages from source
+
+;; Copyright (C) 1998 by Free Software Foundation, Inc.
+
+;; Author: SL Baur <steve@altair.xemacs.org>
+;; Keywords: internal, lisp
+
+;; This file is part of XEmacs.
+
+;; XEmacs is free software; you can redistribute it and/or modify it
+;; under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 2, or (at your option)
+;; any later version.
+
+;; XEmacs is distributed in the hope that it will be useful, but
+;; WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+;; General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with XEmacs; see the file COPYING.  If not, write to the Free
+;; Software Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA
+;; 02111-1307, USA.
+
+;;; Synched up with: Not in FSF
+
+;;; Commentary:
+
+;; This is meant to be called -batch and handles setting up the loadpath
+;; and minimizing what autoloads are imported.
+
+;; This package cannot be used with an XEmacs binary that was built with
+;; backwards compatible lisp directories.
+
+;;; Code:
+
+;; (setq stack-trace-on-error t)
+
+(when (interactive-p)
+  (error "package-compile may only be used with -batch"))
+
+;;; Step 1, purify the load-path and load the core autoloads.
+
+(when (> (length load-path) 2)
+  (setq load-path (last load-path 2)))
+
+(load (expand-file-name "auto-autoloads" (car load-path)))
+
+;;; Step 2, collect the dependencies into load-path and load the autoloads.
+(require 'bytecomp)
+
+(defconst package-directory-map
+  '(
+    ;; libs/*
+    ("Sun" . "libs")
+    ("apel" . "libs")
+    ("edebug" . "libs")
+    ("efs" . "libs")
+    ("mail-lib" . "libs")
+    ("tooltalk" . "libs")
+    ("xemacs-base" . "libs")
+    ("xemacs-devel" . "libs")
+    ;; mule/*
+    ("mule-base" . "mule")
+    ("egg-its" . "mule")
+    ("leim" . "mule")
+    ("locale" . "mule")
+    ("skk" . "mule")
+    ;; prog/*
+    ("ada" . "prog")
+    ("c-support" . "prog")
+    ("cc-mode" . "prog")
+    ("debug" . "prog")
+    ("ediff" . "prog")
+    ("emerge" . "prog")
+    ("pcl-cvs" . "prog")
+    ("prog-modes" . "prog")
+    ("scheme" . "prog")
+    ("sh-script" . "prog")
+    ("vc" . "prog")
+    ("vc-cc" . "prog")
+    ("vhdl" . "prog")
+    ;; comm/*
+    ("footnote" . "comm")
+    ("gnats" . "comm")
+    ("gnus" . "comm")
+    ("mailcrypt" . "comm")
+    ("mh-e" . "comm")
+    ("net-utils" . "comm")
+    ("ph" . "comm")
+    ("rmail" . "comm")
+    ("supercite" . "comm")
+    ("tm" . "comm")
+    ("vm" . "comm")
+    ("w3" . "comm")
+    ;; games/*
+    ("cookie" . "games")
+    ("games" . "games")
+    ("mine" . "games")
+    ("misc-games" . "games")
+    ;; oa/*
+    ("calendar" . "oa")
+    ("edit-utils" . "oa")
+    ("forms" . "oa")
+    ("frame-icon" . "oa")
+    ("hm--html-menus" . "oa")
+    ("ispell" . "oa")
+    ("pc" . "oa")
+    ("psgml" . "oa")
+    ("sgml" . "oa")
+    ("slider" . "oa")
+    ("speedbar" . "oa")
+    ("strokes" . "oa")
+    ("text-modes" . "oa")
+    ("time" . "oa")
+    ;; os/*
+    ("eterm" . "os")
+    ("igrep" . "os")
+    ("ilisp" . "os")
+    ("os-utils" . "os")
+    ("view-process" . "os")
+    ;; wp/*
+    ("auctex" . "wp")
+    ("crisp" . "wp")
+    ("edt" . "wp")
+    ("texinfo" . "wp")
+    ("textools" . "wp")
+    ("tpu" . "wp")
+    ("viper" . "wp")))
+
+(defun package-name-to-directory (package)
+  "Map `package' to a source directory."
+  (let ((dir (expand-file-name
+	      package
+	      (expand-file-name (cdr (assoc package
+					    package-directory-map))
+				"../.."))))
+    (cond ((equal package "gnus")
+	   (expand-file-name "gnus/lisp" (file-name-as-directory dir)))
+	  ((equal package "w3")
+	   (expand-file-name "lisp" (file-name-as-directory dir)))
+	  (t dir))))
+
+(defvar depends nil)
+(defvar command-line-args-left)
+
+(while (and command-line-args-left
+	    (not (equal "--" (car command-line-args-left))))
+  (push (car command-line-args-left) depends)
+  (pop command-line-args-left))
+
+(when command-line-args-left
+  (pop command-line-args-left))
+
+;; Setup load-path and load necessary auto-autoloads
+(while depends
+  (let ((dir (package-name-to-directory (car depends))))
+    (push dir load-path)
+    (load (expand-file-name "auto-autoloads" dir))
+    (pop depends)))
+
+;; Lastly, add the current directory
+(push default-directory load-path)
+
+;;; Step 3, perform the requested bytecompilation
+
+;; (prin1 "Load path = ")
+;; (prin1 load-path)
+;; (terpri)
+
+(batch-byte-compile)
+
+(provide 'package-compile)
+
+;;; package-compile.el ends here
